3fec0440d303303d7f7a440e3c2b571bbd4abf47
BinzXD <starswars164@gmail.com>
2024-08-16 01:24:52 +0700

    login

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,40 @@
 import 'package:flutter/material.dart';
-import 'login_page.dart';
+import 'package:sertifikasi_jmp_kp3/home_page.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:sertifikasi_jmp_kp3/user_auth/registerpage.dart';
+import 'package:sertifikasi_jmp_kp3/user_auth/login_page.dart';
+import 'package:sertifikasi_jmp_kp3/user_auth/sign_up.dart';
+import 'package:sertifikasi_jmp_kp3/user_auth/splash_screen.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:sertifikasi_jmp_kp3/firebase_options.dart';
 
-void main() {
+
+
+Future main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp( 
-      title: 'Cake and Bakery Shop',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: LoginPage(),
-      debugShowCheckedModeBanner: false,
+    return MaterialApp(
+      title: 'NutriScan',
+      theme: ThemeData(useMaterial3: true, colorSchemeSeed: Colors.orange),
+      routes: {
+        '/': (context) => SplashScreen(),
+        '/login': (context) => LoginPage(),
+        '/signUp': (context) => SignUpPage(),
+        // '/setting': (context) => SettingPage(),
+        // '/home': (context) => HomePage(),
+      },
     );
   }
 }
+
+
