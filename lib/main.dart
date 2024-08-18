b4da4e6ce9247ff94e72a3135421216a483f2798
Rafi Adi Pramana <rafiadipramana@gmail.com>
2024-08-18 20:02:23 +0700

    feat(main): add some changes to main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,40 +1,42 @@
 import 'package:flutter/material.dart';
-import 'package:sertifikasi_jmp_kp3/home_page.dart';
+import 'package:sertifikasi_jmp_kp3/ui/home/home_page.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'package:sertifikasi_jmp_kp3/user_auth/registerpage.dart';
-import 'package:sertifikasi_jmp_kp3/user_auth/login_page.dart';
-import 'package:sertifikasi_jmp_kp3/user_auth/sign_up.dart';
-import 'package:sertifikasi_jmp_kp3/user_auth/splash_screen.dart';
-import 'package:firebase_auth/firebase_auth.dart';
-import 'package:sertifikasi_jmp_kp3/firebase_options.dart';
-
-
+import 'package:sertifikasi_jmp_kp3/ui/map/map_page.dart';
+import 'package:sertifikasi_jmp_kp3/ui/auth/login_page.dart';
+import 'package:sertifikasi_jmp_kp3/ui/main/main_page.dart';
+import 'package:sertifikasi_jmp_kp3/ui/user_auth/sign_up.dart';
+import 'package:sertifikasi_jmp_kp3/ui/user_auth/splash_screen.dart';
+import 'package:sertifikasi_jmp_kp3/config/firebase_options.dart';
 
 Future main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
-  runApp(MyApp());
+  runApp(
+    const MyApp(),
+  );
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Cake by Ranggo',
-      theme: ThemeData(useMaterial3: true, colorSchemeSeed: Colors.orange),
+      theme: ThemeData(
+        useMaterial3: true,
+        colorSchemeSeed: Colors.orange,
+      ),
       routes: {
-        '/': (context) => SplashScreen(),
-        '/login': (context) => LoginPage(),
-        '/signUp': (context) => SignUpPage(),
-        // '/setting': (context) => SettingPage(),
-        '/home': (context) => HomePage(role: 'user'),
+        '/': (context) => const SplashScreen(),
+        '/login': (context) => const LoginPage(),
+        '/signUp': (context) => const SignUpPage(),
+        '/main': (context) => const MainPage(),
+        '/map': (context) => const MapPage(),
       },
     );
   }
 }
-
-
