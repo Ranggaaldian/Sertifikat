2c1fa77d6ca9e1958a1ccd76b76e359e0d8f82cd
Rafi Adi Pramana <rafiadipramana@gmail.com>
2024-08-19 20:10:54 +0700

    feat(map): remove location pointing to fix bug

--- lib/ui/map/map_page.dart
+++ lib/ui/map/map_page.dart
@@ -1,90 +1,26 @@
 import 'package:flutter/material.dart';
-import 'package:geolocator/geolocator.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
-class MapPage extends StatefulWidget {
+class MapPage extends StatelessWidget {
   const MapPage({super.key});
 
   @override
-  State<MapPage> createState() => _MapPageState();
-}
-
-class _MapPageState extends State<MapPage> {
-  bool? isLoading;
-  bool? isLocationServiceEnabled;
-  LocationPermission? locationPermission;
-  LatLng? currentLocation;
-
-  @override
-  void initState() {
-    super.initState();
-    _checkGpsService();
-  }
-
-  Future<void> _checkGpsService() async {
-    // Memeriksa apakah layanan lokasi diaktifkan
-    isLocationServiceEnabled = await Geolocator.isLocationServiceEnabled();
-    // if (!isLocationServiceEnabled!) {
-    //   return;
-    // }
-
-    // Memeriksa izin lokasi
-    locationPermission = await Geolocator.checkPermission();
-    if (locationPermission == LocationPermission.denied) {
-      locationPermission = await Geolocator.requestPermission();
-      if (locationPermission == LocationPermission.denied) {
-        return;
-      }
-    }
-
-    if (locationPermission == LocationPermission.deniedForever) {
-      return;
-    }
-
-    if (isLocationServiceEnabled! &&
-        (locationPermission == LocationPermission.whileInUse ||
-            locationPermission == LocationPermission.always)) {
-      _determinePosition();
-    }
-  }
-
-  Future<void> _determinePosition() async {
-    // Mendapatkan lokasi saat ini
-    Position position = await Geolocator.getCurrentPosition(
-        desiredAccuracy: LocationAccuracy.best);
-
-    setState(() {
-      currentLocation = LatLng(position.latitude, position.longitude);
-    });
-  }
-
-  @override
   Widget build(BuildContext context) {
-    if (isLocationServiceEnabled != null &&
-        (locationPermission == LocationPermission.whileInUse ||
-            locationPermission == LocationPermission.always)) {
-      return GoogleMap(
-        initialCameraPosition: CameraPosition(
-          target: currentLocation!,
-          zoom: 15,
-        ),
-        markers: {
-          Marker(
-            icon: BitmapDescriptor.defaultMarker,
-            markerId: const MarkerId('currentLocation'),
-            position: currentLocation!,
-            infoWindow: const InfoWindow(
-              title: 'Lokasi Anda',
-            ),
+    return GoogleMap(
+      initialCameraPosition: const CameraPosition(
+        target: LatLng(-7.0521, 110.4353),
+        zoom: 15,
+      ),
+      markers: {
+        const Marker(
+          icon: BitmapDescriptor.defaultMarker,
+          markerId: MarkerId('currentLocation'),
+          position: LatLng(-7.0521, 110.4353),
+          infoWindow: InfoWindow(
+            title: 'Lokasi Toko Roti',
           ),
-        },
-      );
-    } else {
-      return const Scaffold(
-        body: Center(
-          child: CircularProgressIndicator(),
         ),
-      );
-    }
+      },
+    );
   }
 }
